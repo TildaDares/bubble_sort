74767caa5ee4eca45e87a840a69e2462a5a49487
Tilda Udufo <mathildaudufo@gmail.com>
2020-07-19 15:58:21 +0100

    Refactored

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,31 +1,30 @@
-def sort_array(bubble_array) 
-    for i in (0..bubble_array.length-1) 
-        swapped = false
-        for j in (0...bubble_array.length-i-1) 
-            if bubble_array[j] > bubble_array[j+1]
-                temp = bubble_array[j]
-                bubble_array[j] = bubble_array[j+1]
-                bubble_array[j+1] = temp
-                swapped = true
-            end
-        end
-        if swapped == false then break
-        end
+# frozen_string_literal: true
+
+def sort_array(bubble_array)
+  (0...bubble_array.length).each do |i|
+    swapped = false
+    (0...bubble_array.length - i - 1).each do |j|
+      next unless bubble_array[j] > bubble_array[j + 1]
+
+      temp = bubble_array[j]
+      bubble_array[j] = bubble_array[j + 1]
+      bubble_array[j + 1] = temp
+      swapped = true
     end
-    puts bubble_array
+    break unless swapped
+  end
+  puts bubble_array
 end
 
-puts "Enter the array you would like to sort (separate input with a space)"
+puts 'Enter the array you would like to sort (separate input with a space)'
 input = gets.chomp.split(' ')
 
 while input.any?(/\D/)
-    puts "Only digits are allowed"
-    puts "Enter the array you would like to sort (separate prices with a space)"
-    input = gets.chomp.split(' ')
+  puts 'Only digits are allowed'
+  puts 'Enter the array you would like to sort (separate prices with a space)'
+  input = gets.chomp.split(' ')
 end
 
-bubble_array = input.map do |number|
-    number = number.to_i
-end
+bubble_array = input.map(&:to_i)
 
 sort_array(bubble_array)
